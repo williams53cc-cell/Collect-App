68fa8a83b061fa1273f06097520a79ae9e457045
williams53cc-cell <williams53cc@gmail.com>
2026-09-25 11:42:59 -0500

    Update password-input.tsx

--- components/password-input.tsx
+++ components/password-input.tsx
@@ -27,7 +27,7 @@ export function PasswordInput({
         required
         minLength={6}
         autoComplete={autoComplete}
-        className="w-full rounded-md border border-gray-300 px-3 py-2 pr-10 text-sm focus:border-gray-500 focus:outline-none"
+        className="w-full rounded-md border border-gray-300 px-3 py-2 pr-10 text-sm focus:border-blue-500 focus:outline-none"
       />
       <button
         type="button"
